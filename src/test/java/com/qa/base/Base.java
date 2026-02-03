ac018dbc10cc45b31e649900d44bd7014092dc98
athakre311 <athakre311@gmail.com>
2026-02-03 14:59:27 +0530

    create changes in Base class add method to read data from properties file

--- src/test/java/com/qa/base/Base.java
+++ src/test/java/com/qa/base/Base.java
@@ -1,18 +1,31 @@
 package com.qa.base;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Base {
 	WebDriver driver;
-	public WebDriver lounchBrowser() {
+	public Properties prop = new Properties();
+	
+	public WebDriver lounchBrowser(String url) {
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
-		driver.get("https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC");
+		driver.get(url);
 		return driver;
 	}
+	
+	public Base() throws IOException {
+		prop = new Properties();
+		FileInputStream fis  = new FileInputStream("C:\\Users\\Admin\\eclipse-workspace\\SeleniumJAvaTestNG001\\src\\main\\java\\com\\qa\\config\\config.properties");
+		prop.load(fis);
+	}
+	
+	
 
 }
